e9588abefd698909e76ba478e5812abe5c8d0628
Habib <hbrawnak@gmail.com>
2018-04-26 22:24:20 +0600

    vote and result added

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<question_id>/', views.detail, name='detail'),
     path('<question_id>/results', views.results, name='results'),
-    path('<question_id>/votes', views.votes, name='votes'),
+    path('<int:question_id>/vote', views.votes, name='votes'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
-from django.template import loader, RequestContext
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -17,15 +16,22 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = Question.objects.get(pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question':question}
     return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
-    return HttpResponse("Result view %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def votes(request, question_id):
-    return HttpResponse("Votes %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question':question, 'error_message': 'Please select a choice'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
 
-def home(request):
-    return HttpResponse("Home")
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
